f0f6d1dd0d1aded0f73c27f5457923f7b30c2376
axela <axel.antunes@utbm.Fr>
2024-06-03 14:59:36 +0200

    Correction app router view

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './pages/general/home/home.component';
 import { AboutComponent } from './pages/general/about/about.component';
 import { ContactComponent } from './pages/general/contact/contact.component';
 import { LoginComponent } from './pages/general/login/login.component';
 import { SignupComponent } from './pages/general/signup/signup.component';
-
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -13,6 +13,11 @@ export const routes: Routes = [
   { path: 'contact', component: ContactComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: '**', redirectTo: '/login' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `home` route at startup
 ];
 
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
