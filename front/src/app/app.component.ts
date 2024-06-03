ee239dfbaf158ffaf680f76a57f11a1d3f7ded54
axela <axel.antunes@utbm.Fr>
2024-06-03 15:18:33 +0200

    Router on header Debugged

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {CommonModule} from "@angular/common";
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {HeaderComponent} from "./header/header.component";
-import {MainComponent} from "./main/main.component";
 import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, MainComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WE4B-Projet';
+  title = 'routing-app';
 }
