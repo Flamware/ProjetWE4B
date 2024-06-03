94063b521c58dff83e56b6d7e2934aac06fa4084
axela <axel.antunes@utbm.Fr>
2024-06-03 14:39:30 +0200

    - added Header - added Footer - added Main

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {CommonModule} from "@angular/common";
+import {HeaderComponent} from "./header/header.component";
+import {MainComponent} from "./main/main.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProjetWE4B';
+  title = 'routing-app';
 }
--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/general/home/home.component';
+import { AboutComponent } from './pages/general/about/about.component';
+import { ContactComponent } from './pages/general/contact/contact.component';
+import { LoginComponent } from './pages/general/login/login.component';
+import { SignupComponent } from './pages/general/signup/signup.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', redirectTo: '/home' }
+];
+
