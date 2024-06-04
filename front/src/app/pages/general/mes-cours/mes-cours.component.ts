f613c079ac6637d07098a0dd7e4686c9a8e95631
Estouan Gachelin <120207575+estouan@users.noreply.github.com>
2024-06-04 10:02:41 +0200

    rajout de test pour les cours il faudra juste rajouter une fonction pour prendre les valeur depuis la db

--- front/src/app/pages/general/mes-cours/mes-cours.component.ts
+++ front/src/app/pages/general/mes-cours/mes-cours.component.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgOptimizedImage} from "@angular/common";
 
+class cours {
+  id : number=0;
+  nom: string="";
+  description: string="";
+  date: string="";
+  image: string="";
+
+}
 @Component({
   selector: 'app-mes-cours',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgOptimizedImage
   ],
   templateUrl: './mes-cours.component.html',
   styleUrl: './mes-cours.component.css'
 })
+
+
 export class MesCoursComponent {
-  listeCours: any = [1,2,3,4,5]
+  cour1: cours = {id: 1, nom: "cours1", description: "description1", date: "date1", image: "image1"};
+  cour2: cours = {id: 2, nom: "cours2", description: "description2", date: "date2", image: "image2"};
+  cour3: cours = {id: 3, nom: "cours3", description: "description3", date: "date3", image: "image3"};
+  ListeCours: cours[] = [this.cour1, this.cour2, this.cour3];
 
 }
