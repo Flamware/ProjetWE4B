db502b76d6ec3c3a633bbf7521656b2510e3bd11
Olivier <guillaume.py20@gmail.com>
2024-06-14 23:11:30 +0200

    J'ai créé une fonction pour get les données

--- front/src/app/pages/general/home/home.component.ts
+++ front/src/app/pages/general/home/home.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '@auth0/auth0-angular';
 
+type mentor = {
+    firstname: string,
+    lastname: string,
+    email: string,
+    profile_picture?:any
+};
+
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    standalone: true,
+    imports: [CommonModule],
 })
 export class HomeComponent {
-  constructor(public auth: AuthService) {}
+    mentors: mentor[] = [
+        {firstname : "Alice", lastname : "Test", email : "test@test.fr"},
+        {firstname : "Bob", lastname : "Test", email : "test@test.fr"},
+        {firstname : "Charles", lastname : "Test", email : "test@test.fr"},
+        {firstname : "Dorothée", lastname : "Test", email : "test@test.fr"},
+        {firstname : "Eliott", lastname : "Test", email : "test@test.fr"},
+        {firstname : "Fabrice", lastname : "Test", email : "test@test.fr"}
+    ];
+    constructor(public auth: AuthService) {}
+
+    getMentors() {
+        
+    }
 }
