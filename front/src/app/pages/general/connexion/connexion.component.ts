f3dabd5516d3cb6dc77755621d4a246824bc2bb7
estouan <120207575+estouan@users.noreply.github.com>
2024-06-16 22:18:54 +0200

    css register/login

--- front/src/app/pages/general/connexion/connexion.component.ts
+++ front/src/app/pages/general/connexion/connexion.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup ,ReactiveFormsModule,Validators} from '@angular/forms';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.css'
@@ -15,7 +18,7 @@ export class ConnexionComponent {
   formconnexion: FormGroup;
   constructor() {
     this.formconnexion = new FormGroup({
-      email: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.email]),
       password: new FormControl('', [Validators.required]),
     });
 
