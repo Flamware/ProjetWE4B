4e50e05d228fc731fe46a65450c2a67660e93510
Olivier <guillaume.py20@gmail.com>
2024-06-17 19:22:37 +0200

    Ajout d'un formulaire angular sur le profil

--- front/src/app/pages/general/Profile/profile.component.ts
+++ front/src/app/pages/general/Profile/profile.component.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import {NgIf} from "@angular/common";
+
+type userinfo = {
+  first_name: string,
+  last_name: string,
+  email: string,
+  username: string,
+  bio: string,
+  role: 'User',
+  created_at: Date,
+};
 
 @Component({
   selector: 'app-user-profile',
@@ -9,31 +20,46 @@ import {NgIf} from "@angular/common";
   standalone: true,
   imports: [
     NgIf,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
-  userInfo = {
+  userInfo: userinfo = {
     first_name: 'John',
     last_name: 'Doe',
     email: 'john.doe@example.com',
     username: 'johndoe',
     bio: 'Hello, I am John Doe.',
     role: 'User',
-    created_at: new Date('2022-01-01').toLocaleDateString() // S'adapte au pays du client
+    created_at: new Date('2022-01-01') // S'adapte au pays du client
   };
 
+  userData?:FormGroup;
 
   constructor(public auth: AuthService, private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get<any>('http://localhost:3000/getuserinfo')
-      .subscribe((response) => {
-        this.userInfo = response;
+    // this.http.get<any>('http://localhost:3000/getuserinfo')
+    //   .subscribe((response) => {
+    //     this.userInfo = response;
 
-      });
+    //   });
+    this.userData = new FormGroup({
+      username: new FormControl(this.userInfo.username),
+      email: new FormControl(this.userInfo.email),
+      biography: new FormControl(this.userInfo.bio)
+    }); 
+  }
+
+  onSubmit() {
+    if(this.userData === undefined) {
+      return;
+    }
+    console.log(this.userData.value);
+    //TODO: Check les valeurs pour voir si y'a un changement avant de revoyer a la db
   }
 
 
