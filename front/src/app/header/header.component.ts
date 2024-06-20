514661223f77fad30e9fa7c5658caec3f2ad2637
estouan <120207575+estouan@users.noreply.github.com>
2024-06-20 20:22:21 +0200

    enlevage du bouton login quand deja login

--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
@@ -16,6 +16,11 @@ import { AsyncPipe } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isconnected = false;
   constructor() {}
+  ngOnInit(): void {
+    this.isconnected = localStorage.getItem('token') !== null;
+    console.log('isconnected', this.isconnected);
+  }
 }
--- front/src/app/services/auth/auth.service.ts
+++ front/src/app/services/auth/auth.service.ts
@@ -36,4 +36,7 @@ export class AuthService {
   setToken(token: string): void {
     localStorage.setItem('token', token);
   }
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
 }
