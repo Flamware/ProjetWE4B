14c96fa18ac2e66dda86e51a7ffc359f5d5461b6
Olivier <guillaume.py20@gmail.com>
2024-06-21 22:00:31 +0200

    Changement du style de la barre de recherche et ajout du redirect lors du click sur un cours

--- front/src/app/components/recherche/recherche.component.ts
+++ front/src/app/components/recherche/recherche.component.ts
@@ -1,15 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {CourseService} from "../../services/course/course.service";
 import {Observable} from "rxjs";
 import {Course} from "../../models/course";
+import { SlicePipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recherche',
   standalone: true,
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    SlicePipe
   ],
   templateUrl: './recherche.component.html',
   styleUrl: './recherche.component.css'
@@ -18,40 +21,62 @@ export class RechercheComponent implements OnInit{
   listcourses: Course[] = [];
   protected isFocused: boolean | undefined;
   filteredList: Course[] = [];
+  @ViewChild('results') results!: ElementRef;
 
+  constructor( private courseService: CourseService, public router: Router) {
+    this.isFocused = false;
+    this.filteredList = [];
+  }
+
+  ngOnInit(): void {
+    this.courseService.getAllCourses().subscribe({
+      next: (response: any) => {
+        this.listcourses = response.courses;
+        this.filteredList = this.listcourses;
+      },
+      error: (error: any) => {
+        console.error('Error fetching courses:', error);
+      }
+    });
+  }
+
+  onFocus() {
+    this.isFocused = true;
+    console.log('focus');
+    this.setResultsWidth();
+  }
+  
+  onBlur() {
+    setTimeout(() => {
+      this.isFocused = false;
+    }, 200); // Ajout d'un délai pour permettre le clic sur un élément de la liste
+    const searchbar: HTMLInputElement = document.getElementById('searchbar') as HTMLInputElement;
+    searchbar.value = '';
+  }
+  
   onSearch(event: any) {
     const searchValue = event.target.value.toLowerCase();
     this.filteredList = this.listcourses.filter(item =>
       item.title.toLowerCase().startsWith(searchValue)
     );
   }
-  constructor( private courseService: CourseService) {
-    this.isFocused = false;
-    this.filteredList = [];
-
-  }
-
-  ngOnInit(): void {
-
-    this.courseService.getAllCourses().subscribe({
-      next: (response: any) => {
-        this.listcourses = response.courses;
-      },
-      error: (error: any) => {
-        console.error('Error fetching courses:', error);
-      }
-    });
-
-
-  }
-  onFocus() {
-    this.isFocused = true;
-  }
-
-  onBlur() {
+  
+  // solution un peu nulle mais j'ai pas trouvé mieux
+  private setResultsWidth() {
     setTimeout(() => {
-      this.isFocused = false;
-    }, 200); // Ajout d'un délai pour permettre le clic sur un élément de la liste
+      const searchbar: HTMLInputElement = document.getElementById('searchbar') as HTMLInputElement;
+      if(searchbar && this.results) {
+        this.results.nativeElement.style.width = 'calc(' + searchbar.offsetWidth + 'px - 1em)';
+        console.log('width:', this.results.nativeElement.style.width);
+      }
+    }, 10);
+  }
+
+  public goToCourse(courseId: number) {
+    this.router.navigate(['/cours', courseId]);
+    const searchbar: HTMLInputElement = document.getElementById('searchbar') as HTMLInputElement;
+    searchbar.value = '';
+    this.isFocused = false;
   }
 
 }
